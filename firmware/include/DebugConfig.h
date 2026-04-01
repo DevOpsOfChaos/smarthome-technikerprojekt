2825519474f6f8fd03f4df2d3b255a06c61896d4
Manu <mries444@gmail.com>
2026-04-01 17:52:46 +0200

    firmware: add debug configuration header

---
+++ firmware/include/DebugConfig.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// Zentrale Debug-Grundschalter fuer die Basislinie.
+// Weitere Differenzierung kann spaeter gezielt folgen.
+
+constexpr bool DEBUG_AKTIV = true;
+constexpr bool DEBUG_SENSORIK = true;
+constexpr bool DEBUG_KOMMUNIKATION = true;
+constexpr bool DEBUG_AKTOREN = true;
